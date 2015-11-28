c832eded446215e8f979ec4cfc1ab2b44ff18cf1
Nyks06 <vincent.d.vielle+github@gmail.com>
2015-11-28 23:52:15 +0100

    [WIP] - SKeleton of messagesHandler

--- handler.go
+++ handler.go
@@ -1,5 +1,15 @@
 package Logger
 
-func messagesHandler() {
+import "fmt"
 
+func (l *Logger) messagesHandler() {
+	for {
+		select {
+		case m, ok := <-l.messages:
+			if !ok {
+				fmt.Println("[GO-LOGGER - ERROR - Channel of messages is closed")
+			}
+			fmt.Println(m.format)
+		}
+	}
 }
