9c1cb69ef823e06ae1f73982a475875fd85fc760
Nyks06 <vincent.d.vielle+github@gmail.com>
2015-12-02 14:28:06 +0100

    Complete the test file

--- examples/example.go
+++ examples/example.go
@@ -21,32 +21,32 @@ func main() {
 	//You can enable or disable the color display with a simple function
 	//By default, the text is in color. (Configurable only for console output)
 	//You can check in the color display is enabled or not with a function returning a bool
-	// l.DisableColor()
-	// l.EnableColor()
-	// if l.CheckColorStatus() {
-	// 	//Color is enabled
-	// } else {
-	// 	//Color is not enabled
-	// }
+	l.DisableColor()
+	l.EnableColor()
+	if l.CheckColorStatus() {
+		//Color is enabled
+	} else {
+		//Color is not enabled
+	}
 
 	//You also can enable or disable the logger at every moment with a simple function
 	//As you can check the status of the color display, you can check the status of the logger.
 	//When the logger is disabled, messages will be trashed. By default the logger is enabled.
-	// l.Disable()
-	// l.Enable()
-	// if l.CheckStatus() {
-	// 	//Logger is enabled
-	// } else {
-	// 	//Logger is disabled
-	// }
+	l.Disable()
+	l.Enable()
+	if l.CheckStatus() {
+		//Logger is enabled
+	} else {
+		//Logger is disabled
+	}
 
 	//You easily can configure other things like enable or disable file, console or syslog logging.
-	// l.DisableConsoleLogger()
-	// l.DisableFileLogger()
-	// l.DisableSyslogLogger()
-	// l.EnableConsoleLogger()
-	// l.EnableFileLogger()
-	// l.EnableSyslogLogger()
+	l.DisableConsoleLogger()
+	l.DisableFileLogger()
+	l.DisableSyslogLogger()
+	l.EnableConsoleLogger()
+	l.EnableFileLogger()
+	l.EnableSyslogLogger()
 
 	//Finally, you can display message using functions with the name corresponding to the level of log you want.
 	//You can format your log message as if you were using log package function fmt.Printf
